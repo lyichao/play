ece5fdd855223be1bf15e70afe876b2b67be3ff6
“chao <931667248@qq.com>
2019-12-03 22:02:37 +0800

    利用Path 实现酷炫的启动界面

---
+++ app/src/main/java/com/lyichao/play/ui/activity/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.lyichao.play.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.eftimoff.androipathview.PathView;
+import com.lyichao.play.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.pathView)
+    PathView mPathView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+
+        mPathView.getPathAnimator()
+                .delay(10)
+                .duration(2000)
+                .interpolator(new AccelerateDecelerateInterpolator())
+                .listenerEnd(new PathView.AnimatorBuilder.ListenerEnd() {
+                    @Override
+                    public void onAnimationEnd() {
+
+                        jump();
+                    }
+                })
+                .start();
+    }
+
+    private void jump() {
+        startActivity(new Intent(this,MainActivity.class));
+        this.finish();
+    }
+}
